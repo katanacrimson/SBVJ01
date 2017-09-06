856e6be32d0c3f2281190bfff71a2bd945d3f585
Damian Bushong <katana@odios.us>
2017-09-06 13:12:44 -0500

    Minor update - fixing header comments

--- test/SBVJ01.js
+++ test/SBVJ01.js
@@ -3,8 +3,7 @@
 // ---
 // @copyright (c) 2017 Damian Bushong <katana@odios.us>
 // @license MIT license
-// @url <https://github.com/damianb/>
-// @reddit <https://reddit.com/u/katana__>
+// @url <https://github.com/damianb/SBVJ01>
 //
 /*jslint node: true, asi: true */
 'use strict'
