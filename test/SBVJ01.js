8461f6f17d399c501f6298349b73ca8e94763c71
Damian Bushong <katana@odios.us>
2017-09-14 14:56:29 -0500

    Implement write support for SBVJ01.  Fixes #1.

--- test/SBVJ01.js
+++ test/SBVJ01.js
@@ -9,11 +9,131 @@
 'use strict'
 
 const fs = require('fs-extra')
+const path = require('path')
 const { expect } = require('chai')
-const SBVJ01 = require('./../SBVJ01')
 const ConsumableBuffer = require('ConsumableBuffer')
+const ExpandingBuffer = require('ExpandingBuffer')
+const SBVJ01 = require('./../SBVJ01')
 
 describe('SBVJ01', () => {
+	const tmpDir = __dirname + '/tmp'
+
+	describe('SBVJ01.load', () => {
+		it('should be able to read a sample SBVJ01 file (slow running test)', async function() {
+			// this is a lot of parsing - trying to parse an entire player file takes a little bit
+			//   so, we'll adjust slow/timeout accordingly
+			this.slow(5000)
+			this.timeout(20000)
+
+			const filename = __dirname + '/samples/7bb55a32b4a5fb530273d4b954f39d20.player'
+			const expected = JSON.parse(await fs.readFile(__dirname + '/samples/Misty.player', { encoding: 'utf8', flag: 'r' }))
+			let player = new SBVJ01(filename)
+
+			let res = await player.load()
+
+			expect(res.version).to.equal(30)
+			expect(res.entity).to.deep.equal(expected)
+			expect(res.name).to.equal('PlayerEntity')
+		})
+	})
+
+	describe('SBVJ01.save', () => {
+		afterEach(async () => {
+			return // TODO: REMOVE
+			let files = await fs.readdir(tmpDir + '/')
+			for(const file of files) {
+				if(file === '.gitkeep') {
+					continue
+				}
+
+				await fs.unlink(path.join(tmpDir, file))
+			}
+		})
+
+		it('should throw if no entity name was specified', async () => {
+			const filename = tmpDir + '/test.player'
+			let player = new SBVJ01(filename)
+
+			player.name = null
+			player.version = 3
+			player.entity = {
+				testEntity: 100
+			}
+
+			let res = null
+			try {
+				await player.save()
+			} catch (err) {
+				res = err
+			}
+			expect(res).to.be.an.instanceof(Error)
+			expect(res.message).to.equal('An entity name must be specified before attempting to save an SBVJ01 file.')
+		})
+
+		it('should throw if no entity version was specified', async () => {
+			const filename = tmpDir + '/test.player'
+			let player = new SBVJ01(filename)
+
+			player.name = 'TestEntity'
+			player.version = null
+			player.entity = {
+				testEntity: 100
+			}
+
+			let res = null
+			try {
+				await player.save()
+			} catch (err) {
+				res = err
+			}
+			expect(res).to.be.an.instanceof(Error)
+			expect(res.message).to.equal('An entity version must be specified before attempting to save an SBVJ01 file.')
+		})
+
+		it('should work as expected when writing a sample SBVJ01 file', async () => {
+			const filename = tmpDir + '/test.player'
+			let player = new SBVJ01(filename)
+
+			let name = player.name = 'TestEntity'
+			let version = player.version = 5
+			let entity = player.entity = {
+				crazyTest: [1, 2, 'a']
+			}
+			let expectedBuffer = Buffer.from([
+				0x53, 0x42, 0x56, 0x4a, 0x30, 0x31, 0x0a, 0x54,
+				0x65, 0x73, 0x74, 0x45, 0x6e, 0x74, 0x69, 0x74,
+				0x79, 0x01, 0x00, 0x00, 0x00, 0x05, 0x07, 0x01,
+				0x09, 0x63, 0x72, 0x61, 0x7a, 0x79, 0x54, 0x65,
+				0x73, 0x74, 0x06, 0x03, 0x04, 0x02, 0x04, 0x04,
+				0x05, 0x01, 0x61
+			])
+
+			let res = await player.save()
+
+			expect(res.name).to.equal(name)
+			expect(res.version).to.equal(version)
+			expect(res.entity).to.deep.equal(entity)
+
+			expect(Buffer.compare(await fs.readFile(filename), expectedBuffer)).to.equal(0)
+		})
+
+		it('should work as expected when modifying a sample SBVJ01 file', async () => {
+			const filename = tmpDir + '/7bb55a32b4a5fb530273d4b954f39d20.player'
+			await fs.copy(__dirname + '/samples/7bb55a32b4a5fb530273d4b954f39d20.player', filename)
+			let player = new SBVJ01(filename)
+			await player.load()
+
+			// rename the character!
+			player.entity.identity.name = 'Julie'
+
+			let res = await player.save()
+
+			expect(res.entity.identity.name).to.equal('Julie')
+			// final verification would be to load the .player file in Starbound itself and verify the player's name...
+		})
+
+	})
+
 	describe('SBVJ01._readHeader', () => {
 		it('should throw if passed something other than a ConsumableBuffer or ConsumableFile', async () => {
 			let res = null
@@ -85,23 +205,68 @@ describe('SBVJ01', () => {
 			expect(res.version).to.equal(1)
 		})
 	})
-})
 
-describe('SBVJ01 integration test', () => {
-	it('should work as expected on a sample SBVJ01 file (slow)', async function() {
-		// this is a lot of parsing - trying to parse an entire player file takes a little bit
-		//   so, we'll adjust slow/timeout accordingly
-		this.slow(1000)
-		this.timeout(5000)
+	describe('SBVJ01._writeEntity', () => {
+		it('should throw if passed something other than an ExpandingBuffer or ExpandingFile', async () => {
+			let res = null
+			try {
+				await SBVJ01._writeEntity(null)
+			} catch(err) {
+				res = err
+			}
+			expect(res).to.be.an.instanceof(TypeError)
+			expect(res.message).to.equal('SBVJ01._writeEntity expects an ExpandingBuffer or ExpandingFile.')
+		})
 
-		const filename = __dirname + '/samples/7bb55a32b4a5fb530273d4b954f39d20.player'
-		const player = new SBVJ01(filename)
-		const expected = JSON.parse(await fs.readFile(__dirname + '/samples/Misty.player', { encoding: 'utf8', flag: 'r' }))
+		it('should throw if passed something other than a string for the entity name', async () => {
+			let res = null
+			let sbuf = new ExpandingBuffer()
+			try {
+				await SBVJ01._writeEntity(sbuf, null)
+			} catch(err) {
+				res = err
+			}
+			expect(res).to.be.an.instanceof(TypeError)
+			expect(res.message).to.equal('SBVJ01._writeEntity expects the provided entity name to be a string.')
+		})
 
-		let res = await player.load()
+		it('should throw if passed something other than an integer for the entity version', async () => {
+			let res = null
+			let sbuf = new ExpandingBuffer()
+			try {
+				await SBVJ01._writeEntity(sbuf, 'test', null)
+			} catch(err) {
+				res = err
+			}
+			expect(res).to.be.an.instanceof(TypeError)
+			expect(res.message).to.equal('SBVJ01._writeEntity expects the provided entity version to be an integer.')
 
-		expect(res.version).to.equal(30)
-		expect(res.entity).to.deep.equal(expected)
-		expect(res.name).to.equal('PlayerEntity')
+			res = null
+			try {
+				await SBVJ01._writeEntity(sbuf, 'test', 0.5)
+			} catch(err) {
+				res = err
+			}
+			expect(res).to.be.an.instanceof(TypeError)
+			expect(res.message).to.equal('SBVJ01._writeEntity expects the provided entity version to be an integer.')
+		})
+
+		it('should correctly write the SBVJ01 entity', async () => {
+			let sbuf = new ExpandingBuffer()
+			let expectedBuffer = Buffer.from([
+				0x53, 0x42, 0x56, 0x4a, 0x30, 0x31, 0x0a, 0x54,
+				0x65, 0x73, 0x74, 0x45, 0x6e, 0x74, 0x69, 0x74,
+				0x79, 0x01, 0x00, 0x00, 0x00, 0x05, 0x07, 0x01,
+				0x09, 0x63, 0x72, 0x61, 0x7a, 0x79, 0x54, 0x65,
+				0x73, 0x74, 0x06, 0x03, 0x04, 0x02, 0x04, 0x04,
+				0x05, 0x01, 0x61
+			])
+
+			let res = await SBVJ01._writeEntity(sbuf, 'TestEntity', 5, {
+				crazyTest: [1, 2, 'a']
+			})
+
+			expect(Buffer.compare(sbuf.buf, expectedBuffer)).to.equal(0)
+		})
 	})
 })
